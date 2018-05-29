49b98a16e41f28cf11a2de5424803537ad407486
Kevin Boese <kmboese@ucdavis.edu>
2018-05-29 00:58:59 -0700

    Moved functions from project.cc into appropriate header/source code in common.c/common.h files

--- s2client-api/pathfinder/common/common.cc
+++ s2client-api/pathfinder/common/common.cc
@@ -1,6 +1,18 @@
 #include "common.h"
 #include <iostream>
 
+#include "sc2renderer/sc2_renderer.h"
+
+//Scale for window rendering
+//16:9 scale
+//#define SCALE 60
+//4:3 scale
+#define SCALE 240
+const int kMapX = 4 * SCALE;
+const int kMapY = 3 * SCALE;
+const int kMiniMapX = 220;
+const int kMiniMapY = 200;
+
 //Original
 //using namespace sc2;
 namespace sc2 {
@@ -31,4 +43,191 @@ Point2DI ConvertWorldToMinimap(const GameInfo& game_info, const Point2D& world) 
 
     return Point2DI(image_x, image_y);
 }
+
+void PathingBot::OnGameStart() {
+    renderer::Initialize("Rendered", 50, 50, kMiniMapX + kMapX, std::max(kMiniMapY, kMapY));
+}
+
+void PathingBot::OnStep() {
+    uint32_t gameLoop = Observation()->GetGameLoop();
+
+    //Periodically print game info
+    if (gameLoop % 100 == 0) {
+        PrintMinerals();
+    }
+
+    // moveCamera();
+    TryBuildSupplyDepot();
+    TryBuildBarracks();
+    /*
+    //Temporarily removed Render()
+    Render();
+    */
+}
+
+void PathingBot::OnGameEnd() {
+    renderer::Shutdown();
+}
+
+void PathingBot::OnUnitIdle(const Unit* unit) {
+    switch (unit->unit_type.ToType()) {
+        case UNIT_TYPEID::TERRAN_COMMANDCENTER: {
+            if (CountUnitType(UNIT_TYPEID::TERRAN_SCV) < OPTIMAL_SCV_COUNT) {
+                Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_SCV);
+                std::cout << "Trained SCV\n";
+            }
+            break;
+        }
+        case UNIT_TYPEID::TERRAN_SCV: {
+            const Unit* mineral_target = FindNearestMineralPatch(unit->pos);
+            if (!mineral_target) {
+                break;
+            }
+            Actions()->UnitCommand(unit, ABILITY_ID::SMART, mineral_target);
+            break;
+        }
+                                      //Train marines in groups (wait until we have a pool of resources to begin building)
+        case UNIT_TYPEID::TERRAN_BARRACKS: {
+            Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_MARINE);
+            break;
+        }
+        case UNIT_TYPEID::TERRAN_MARINE: {
+            const GameInfo& game_info = Observation()->GetGameInfo();
+            Actions()->UnitCommand(unit, ABILITY_ID::ATTACK_ATTACK, game_info.enemy_start_locations.front());
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+}
+
+/*Attempt to build a given structure
+ * Note: default argument can only be given in header declaration
+*/
+bool PathingBot::TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type) {
+    const ObservationInterface* observation = Observation();
+    int unit_count = 0; //How many units are chosen to build structures
+
+                        // If a unit already is building a supply structure of this type, do nothing.
+                        // Also get an scv to build the structure.
+    const Unit* unit_to_build = nullptr;
+    Units units = observation->GetUnits(Unit::Alliance::Self);
+    for (const auto& unit : units) {
+        //Select SCV
+        if (unit->unit_type == unit_type) {
+            unit_to_build = unit;
+            //if an SCV is currently building this structure, don't build it
+            for (const auto& order : unit->orders) {
+                if (order.ability_id == ability_type_for_structure) {
+                    return false;
+                }
+            }
+            break;
+        }
+    }
+
+    float rx = GetRandomScalar();
+    float ry = GetRandomScalar();
+
+    //Don't build buildings near minerals
+    float mineral_threshold = 5.0f;
+    const Unit* nearest_minerals = FindNearestMineralPatch(unit_to_build->pos);
+    Point2D build_location{ unit_to_build->pos.x + rx * BUILD_RADIUS, unit_to_build->pos.y + ry * BUILD_RADIUS };
+    while (DistanceSquared2D(build_location, nearest_minerals->pos) < mineral_threshold) {
+        rx = GetRandomScalar();
+        ry = GetRandomScalar();
+        build_location.x = unit_to_build->pos.x + rx * BUILD_RADIUS;
+        build_location.y = unit_to_build->pos.y + ry * BUILD_RADIUS;
+        nearest_minerals = FindNearestMineralPatch(unit_to_build->pos);
+    }
+
+    Actions()->UnitCommand(unit_to_build,
+        ability_type_for_structure,
+        Point2D(unit_to_build->pos.x + rx * BUILD_RADIUS, unit_to_build->pos.y + ry * BUILD_RADIUS));
+    return true;
+}
+
+bool PathingBot::TryBuildSupplyDepot() {
+    const ObservationInterface* observation = Observation();
+
+    // If we are not supply capped, don't build a supply depot.
+    if (observation->GetFoodUsed() <= observation->GetFoodCap() - SUPPLY_BUFFER)
+        return false;
+
+    // Try and build a depot. Find a random SCV and give it the order.
+    return TryBuildStructure(ABILITY_ID::BUILD_SUPPLYDEPOT);
+}
+
+const Unit* PathingBot::FindNearestMineralPatch(const Point2D& start) {
+    Units units = Observation()->GetUnits(Unit::Alliance::Neutral);
+    float distance = std::numeric_limits<float>::max();
+    const Unit* target = nullptr;
+    for (const auto& u : units) {
+        if (u->unit_type == UNIT_TYPEID::NEUTRAL_MINERALFIELD) {
+            float d = DistanceSquared2D(u->pos, start);
+            if (d < distance) {
+                distance = d;
+                target = u;
+            }
+        }
+    }
+    return target;
+}
+
+bool PathingBot::TryBuildBarracks() {
+    const ObservationInterface* observation = Observation();
+
+    if (CountUnitType(UNIT_TYPEID::TERRAN_SUPPLYDEPOT) < 1) {
+        return false;
+    }
+
+    if (CountUnitType(UNIT_TYPEID::TERRAN_BARRACKS) > OPTIMAL_BARRACKS_COUNT) {
+        return false;
+    }
+
+    return TryBuildStructure(ABILITY_ID::BUILD_BARRACKS);
+}
+
+void PathingBot::PrintMinerals() {
+    std::cout << "Minerals: " << Observation()->GetMinerals() << std::endl;
+}
+
+//Returns the number of a given type of unit/building
+size_t PathingBot::CountUnitType(UNIT_TYPEID unit_type) {
+    return Observation()->GetUnits(Unit::Alliance::Self, IsUnit(unit_type)).size();
+}
+
+/* ***** NOT WORKING/NOT USED FUNCTIONS***** */
+//Not working
+void PathingBot::MoveCamera() {
+    // const GameInfo& game_info = Observation()->GetGameInfo();
+
+    // Point2D cameraLocation = Observation()->GetCameraPos();
+    // std::cout << "Camera location: " << cameraLocation.x << " " << cameraLocation.y << std::endl;
+    // Point2D newLocation(cameraLocation.x + 20, cameraLocation.y + 20);
+    // // Point2D target = Observation()->GetStartLocation() + Point2D(3.0f, 3.0f);
+    // Point2DI targetMinimap = ConvertWorldToMinimap(game_info, newLocation);
+    // std::cout << "New Camera location: " << targetMinimap.x << " " << targetMinimap.y << std::endl;
+
+    // ActionsFeatureLayer()->CameraMove(targetMinimap);
+}
+
+//Not used
+/*
+void PathingBot::Render() {
+    const SC2APIProtocol::Observation* observation = Observation()->GetRawObservation();
+    const SC2APIProtocol::ObservationRender& render = observation->render_data();
+
+    const SC2APIProtocol::ImageData& minimap = render.minimap();
+    renderer::ImageRGB(&minimap.data().data()[0], minimap.size().x(), minimap.size().y(), 0, std::max(kMiniMapY, kMapY) - kMiniMapY);
+
+    const SC2APIProtocol::ImageData& map = render.map();
+    renderer::ImageRGB(&map.data().data()[0], map.size().x(), map.size().y(), kMiniMapX, 0);
+
+    renderer::Render();
+}
+*/
+
+
 }
--- s2client-api/pathfinder/common/common.h
+++ s2client-api/pathfinder/common/common.h
@@ -1,5 +1,38 @@
 #include "sc2api/sc2_api.h"
+#include <iostream>
+
+#include "sc2renderer/sc2_renderer.h"
+
+
+#define LINUX_USE_SOFTWARE_RENDER 0
+// How many workers to have built at all times
+#define OPTIMAL_SCV_COUNT 14
+#define OPTIMAL_BARRACKS_COUNT 6
+// How close we can get to our supply cap before building more supply depots
+#define SUPPLY_BUFFER 6
+//How far away from their position SCV's will build structures
+#define BUILD_RADIUS 15.0f
 
 namespace sc2 {
+class PathingBot : public Agent {
+public:
+    virtual void OnGameStart() final;
+    virtual void OnStep() final;
+    virtual void OnUnitIdle(const Unit* unit) final;
+    virtual void OnGameEnd() final;
+
+private:
+    // Currently doesn't work
+    void MoveCamera();
+    //Output the current number of minerals the player has
+    void PrintMinerals();
+    void Render();
+    size_t CountUnitType(UNIT_TYPEID unit_type);
+
+    bool TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV);
+    bool TryBuildSupplyDepot();
+    const Unit* FindNearestMineralPatch(const Point2D& start);
+    bool TryBuildBarracks();
+};
 Point2DI ConvertWorldToMinimap(const GameInfo& game_info, const Point2D& world);
 }
