16114861aa1938010044d95f8cccd7863ffc9672
christinetech <cbtech@ucdavis.edu>
2018-05-31 18:58:47 -0700

    Working create source -- based on source count

--- s2client-api/pathfinder/inf_maps/infMapTester.cpp
+++ s2client-api/pathfinder/inf_maps/infMapTester.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
@@ -5,9 +6,17 @@
 #include "InfluenceMap.h"
 
 void TEST_INIT_MAP(int rows, int cols);
+void TEST_CREATE_SOURCE(InfluenceMap &map);
 
 int main() {
+    std::cout << "Testing initMap\n" << std::endl;
     TEST_INIT_MAP(4, 3);
+
+    std::cout << "\nTesting createSource\n" << std::endl;
+
+    InfluenceMap map(10, 10);
+    map.initMap();
+    TEST_CREATE_SOURCE(map);
     
     return 0;
 }
@@ -15,5 +24,25 @@ int main() {
 void TEST_INIT_MAP(int rows, int cols) {
     InfluenceMap map(rows, cols);
     map.initMap();
+    assert(map.getNumRows() == (unsigned int)rows);
+    assert(map.getNumCols() == (unsigned int)cols);
+
     map.printMap();
+}
+
+void TEST_CREATE_SOURCE(InfluenceMap &map) {
+    unsigned int numSources = 2;
+    Point pt1;
+    pt1.x = 5;
+    pt1.y = 4;
+    map.createSource(pt1, 3.5);
+
+    Point pt2;
+    pt2.x = 1;
+    pt2.y = 4;
+    map.createSource(pt2, 0.2);
+
+    assert(map.getNumSources() == numSources);
+    
+    std::cout << "Num sources: " << map.getNumSources() << std::endl;
 }
